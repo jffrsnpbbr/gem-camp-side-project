a2758c3ee733b3dc6e05ab52f3b98ef5be1d79bf
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-04-20 17:40:24 +0800

    add column coins to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_083226) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_092026) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_20_083226) do
     t.string "username"
     t.integer "role", default: 0
     t.string "phone_number", null: false
+    t.integer "coins", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
