86950120044b85475cbc8f44463494378604abe2
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-04-28 09:56:42 +0800

    generate province model, migration, add validates in model, add has many to province, migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_28_013721) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_015257) do
+  create_table "address_provinces", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.bigint "region_id"
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["region_id"], name: "index_address_provinces_on_region_id"
+  end
+
   create_table "address_regions", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "code"
     t.string "name"
