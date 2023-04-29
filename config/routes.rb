fab16ca8b822c150ac302aebeab786692a2f0b17
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-04-29 15:58:18 +0800

    create regions api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :regions, only: [:index, :show], defaults: { format: :json }
+    end
+  end
   devise_for :users
 
   constraints ClientDomainConstraint.new do
