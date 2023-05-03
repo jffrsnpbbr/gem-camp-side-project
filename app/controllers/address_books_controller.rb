04cd460f6ba040fbf403c9470926e208d09be9cc
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-05-03 13:41:54 +0800

    update address_books use direct user reference instead of user_id, cleanups

--- app/controllers/address_books_controller.rb
+++ app/controllers/address_books_controller.rb
@@ -1,7 +1,6 @@
 class AddressBooksController < ApplicationController
-  before_action :set_user
-  before_action :set_address_book, except: [:index, :new]
-  before_action :sanitize_params
+
+
   def index
     @address_books = AddressBook.includes(
       :user,
@@ -11,13 +10,15 @@ class AddressBooksController < ApplicationController
       :address_barangay
     ).where(user: current_user)
   end
+
   def new
     @address_book = AddressBook.new
   end
 
   def create
     @address_book = AddressBook.new(address_book_params)
-    @address_book.user_id = current_user.id
+    @address_book.user = current_user
+
     if @address_book.save
       redirect_to address_books_path
     else
@@ -26,6 +27,7 @@ class AddressBooksController < ApplicationController
   end
 
   def edit; end
+
   def update
     if @address_book.update(address_book_params)
       redirect_to address_books_path
@@ -35,10 +37,6 @@ class AddressBooksController < ApplicationController
   end
 
   private
-
-  def set_user
-    @user = User.find(current_user.id)
-  end
 
   def set_address_book
     @address_book = AddressBook.includes(
@@ -51,20 +49,15 @@ class AddressBooksController < ApplicationController
   end
 
   def address_book_params
-    puts params.require(:address_book)
     params.require(:address_book).permit(:address_region_id,
                                          :address_province_id,
                                          :address_city_id,
                                          :address_barangay_id,
-                                         :street_address,
+                                         :address_street,
                                          :phone_number,
+                                         :genre,
                                          :remark,
                                          :is_default,
                                          :name)
   end
-
-  def sanitize_params
-    params[:genre] = params[:genre].to_i
-  end
-
 end
