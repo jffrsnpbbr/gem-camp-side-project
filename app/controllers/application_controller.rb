4e7fa798159621171612f8e0d0d50d12d3e75f8b
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-05-10 18:57:51 +0800

    remove devise params from applicatoin controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :phone_number, :password)}
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:image, :username, :email, :phone_number, :password, :current_password)}
-      devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :password, :remember_me)}
-    end
 end
