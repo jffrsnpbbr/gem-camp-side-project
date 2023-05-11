69bac5d847f885c1c49f4578156bf314d342f016
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-05-11 21:19:55 +0800

    fix typo enum, add basic aasm

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,30 @@
 class Item < ApplicationRecord
+  include AASM
+
   default_scope { where(deleted_at: nil )}
 
-  num status: { inactive: 0, active: 1 }
+  enum status: { inactive: 0, active: 1 }
+  
+  aasm column: :state do
+    state :pending, initial: true
+    state :starting, :paused, :ended, :cancelled
+
+    event :start do
+      transitions from: [:pending, :paused, :ended, :cancelled], to: :starting
+    end
+
+    event :pause do
+      transitions from: :starting, to: :paused
+    end
+
+    event :end do
+      transitions from: :starting, to: :ended
+    end
+
+    event :cancel do
+      transitions from: [:starting,:paused], to: :cancel
+    end
+  end
   
   def destroy
     update(deleted_at: Time.current)
