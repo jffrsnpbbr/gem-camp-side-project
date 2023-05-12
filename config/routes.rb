ac408c67208f79e4db13a0e1c3e6dd97485ff044
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-05-12 12:15:19 +0800

    categories crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,21 @@
 Rails.application.routes.draw do
-
   namespace :api do
     namespace :v1 do
-      resources :regions, only: [:index, :show], defaults: { format: :json } do
+      resources :regions, only: %i[index show], defaults: { format: :json } do
         resources :provinces, only: :index, defaults: { format: :json }
       end
 
-      resources :provinces, only: [:index, :show], defaults: { format: :json } do
+      resources :provinces, only: %i[index show], defaults: { format: :json } do
         resources :cities, only: :index, defaults: { format: :json }
       end
 
-      resources :cities, only: [:index, :show], defaults: { format: :json } do
+      resources :cities, only: %i[index show], defaults: { format: :json } do
         resources :barangays, only: :index, defaults: { format: :json }
       end
 
-      resources :barangays, only: [:index, :show], defaults: { format: :json }
+      resources :barangays, only: %i[index show], defaults: { format: :json }
     end
   end
-  
 
   constraints ClientDomainConstraint.new do
     root controller: :home, action: :index
@@ -31,7 +29,7 @@ Rails.application.routes.draw do
       namespace :users do
         resource do
           get :sign_in, action: :new
-          get :edit_user_registration, action: :edit 
+          get :edit_user_registration, action: :edit
         end
       end
 
@@ -46,6 +44,7 @@ Rails.application.routes.draw do
       root controller: :home, action: :index
 
       resources :users, only: :index
+      resources :categories
     end
   end
 end
