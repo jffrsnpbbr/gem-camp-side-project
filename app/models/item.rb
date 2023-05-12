95c6c8ecd62d63b28579c509bda0005fd4f15f69
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-05-12 10:17:46 +0800

    create model item_category_ship, migrate

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,11 @@
 class Item < ApplicationRecord
-  default_scope { where(deleted_at: nil )}
+  default_scope { where(deleted_at: nil) }
 
-  num status: { inactive: 0, active: 1 }
-  
+  enum status: { inactive: 0, active: 1 }
+
+  has_many :item_category_ships
+  has_many :categories, through: :item_category_ships
+
   def destroy
     update(deleted_at: Time.current)
   end
