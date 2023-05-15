799f1a5d534e8c2d08c4be1873d32004b0c2f1c2
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-05-15 11:34:26 +0800

    update admin bet index page, add search feature

--- app/controllers/admin/bets_controller.rb
+++ app/controllers/admin/bets_controller.rb
@@ -1,5 +1,17 @@
 class Admin::BetsController < ApplicationController
+  include ActiveRecord::Sanitization::ClassMethods
   def index
-    @bets = Bet.includes(:user, :item).all
+    @bets = if params[:search].blank?
+              Bet.includes(:user, :item).all
+            else
+              Bet.joins(:user, :item)
+                 .where('
+                    `serial_number` LIKE :search OR
+                    `items`.`name` LIKE :search OR
+                    `users`.`email` LIKE :search', search: "%#{sanitize_sql_like(params[:search])}%")
+            end
+    @bets = @bets.where(state: params[:state])
+    @bets = @bets.where('`created_at` >= ?', params[:start_date]) if params[:start_date].present?
+    @bets = @bets.where('`created_at` <= ?', params[:end_date]) if params[:end_date].present?
   end
 end
